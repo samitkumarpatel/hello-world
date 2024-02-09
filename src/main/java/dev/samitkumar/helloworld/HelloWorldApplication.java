f988ddc5cb011685e4fa973bdf09a84e183f7c60
samitkumarpatel <samitkumarpatel@gmail.com>
2024-02-09 21:07:30 +0100

    Memory leak example added

--- src/main/java/dev/samitkumar/helloworld/HelloWorldApplication.java
+++ src/main/java/dev/samitkumar/helloworld/HelloWorldApplication.java
@@ -9,6 +9,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -19,6 +21,8 @@ public class HelloWorldApplication {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 	private static Integer count = 0;
+	private static List<int[]> listOfArrays = new ArrayList<>();
+
 
 	@Value("${spring.application.version}")
 	private String version;
@@ -30,6 +34,17 @@ public class HelloWorldApplication {
 				.GET("/count", request -> ServerResponse.ok().bodyValue(Map.of("count", count)))
 				.GET("/", request -> {
 					++count;
+
+					int[] array = new int[1000000]; // Creating an array of 1 million integers
+					listOfArrays.add(array); // Adding the array to the list
+
+					// Sleep for a short period to slow down the process
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
 					var reply = Map.of("message", "Hello World", "version", version);
 					return ServerResponse.ok().bodyValue(reply);
 				})
